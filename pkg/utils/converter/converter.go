2be693fb9067fac1108ba234197ecb9c8b2aeaf3
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-01-14 21:16:57 +0800

    Update converter.go

--- pkg/utils/converter/converter.go
+++ pkg/utils/converter/converter.go
@@ -9,7 +9,9 @@ func ConvertHTML2Markdown(html string) (string, error) {
 	converter := md.NewConverter("", true, nil)
 	md, err := converter.ConvertString(html)
 	if err != nil {
-		logrus.Error(err)
+		logrus.WithFields(logrus.Fields{
+			"err": err,
+		}).Error("笔记内容 HTML 转换到 MD 时错误")
 		return "", err
 	}
 	return md, nil
