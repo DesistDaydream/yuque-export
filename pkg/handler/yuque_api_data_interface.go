1901b9412ca3f9bb23a09dfdd2e305d1f4a917c0
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-01-24 17:59:16 +0800

    Update yuque_api_data_interface.go

--- pkg/handler/yuque_api_data_interface.go
+++ pkg/handler/yuque_api_data_interface.go
@@ -8,6 +8,6 @@ type YuqueDataHandler interface {
 	// 处理 API 响应的数据
 	Handle(h *HandlerObject) error
 	// 参考一下 go 实现的 tcping 项目，要不要再几个方法？~
-	// Result() *Result // 直接返回从 API 获取到的结果
+	// Result() *Result // 返回某个通用的结果
 	// SetTarget(target *Target) // 设置一下要处理的 API 目标，比如设定 url 路径之类的
 }
