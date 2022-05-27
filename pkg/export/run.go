da85e5fcadd6bbeb771d2b98c8dee6c8cb8640a5
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-05-28 00:04:05 +0800

    Update run.go

--- pkg/export/run.go
+++ pkg/export/run.go
@@ -19,12 +19,12 @@ func ExportSet(h *handler.HandlerObject, tocs []yuque.TOC) {
 	var wg sync.WaitGroup
 	defer wg.Wait()
 	// 控制并发
-	concurrenceControl := make(chan bool, h.Flags.Concurrency)
+	concurrencyControl := make(chan bool, h.Flags.Concurrency)
 
 	// 逐一导出节点内容
 	for _, toc := range tocs {
 		// 控制并发
-		concurrenceControl <- true
+		concurrencyControl <- true
 		// 并发
 		wg.Add(1)
 
@@ -62,7 +62,7 @@ func ExportSet(h *handler.HandlerObject, tocs []yuque.TOC) {
 			}
 
 			// 控制并发
-			<-concurrenceControl
+			<-concurrencyControl
 		}(toc)
 
 		// 控制并发。。。。。接口请求多了。。。直接限流了。。。囧
@@ -75,7 +75,7 @@ func ExportAll(h *handler.HandlerObject, tocs []yuque.TOC) {
 	var wg sync.WaitGroup
 	defer wg.Wait()
 
-	concurrenceControl := make(chan bool, h.Flags.Concurrency)
+	concurrencyControl := make(chan bool, h.Flags.Concurrency)
 
 	for _, toc := range tocs {
 		// TODO: 当每个 TOC 信息中，data.child_uuid 字段不为空时，为其创建同名文件夹。以便更好分类
@@ -83,7 +83,7 @@ func ExportAll(h *handler.HandlerObject, tocs []yuque.TOC) {
 		// 这个逻辑有点像递归函数，一遍一遍循环 []TOC，每次循环都处理 depth + 1 的字段，凡是 child_uuid 不为空的就创建目录，根据其 parent_uuid 决定该文档写入到哪个目录中。
 		// 这种循环好么？~上千篇文档，循环好多遍，感觉很浪费，如何在循环后就去掉这些数据呢？~
 		// TODO: 同时，添加可选项，是否创建文件夹以分类文档
-		concurrenceControl <- true
+		concurrencyControl <- true
 
 		wg.Add(1)
 
@@ -113,7 +113,7 @@ func ExportAll(h *handler.HandlerObject, tocs []yuque.TOC) {
 				}
 			}
 
-			<-concurrenceControl
+			<-concurrencyControl
 		}(toc)
 
 		time.Sleep(time.Duration(h.Flags.ExportDuration) * time.Second)
@@ -146,23 +146,23 @@ func GetDocDetail(h *handler.HandlerObject, tocs []yuque.TOC) ExceptionDocs {
 		go func(toc yuque.TOC) {
 			defer wg.Done()
 
-			// 获取 Doc 详情
 			docDetail := yuque.NewDocDetail()
+			// 获取 Doc 详情数据
 			if err := docDetail.Get(h, toc.Slug); err != nil {
 				logrus.WithFields(logrus.Fields{
 					"doc": docDetail.Data.Title,
 					"err": err,
 				}).Error("获取文档详情失败!")
 			} else {
-				// 获取文档公开状态
-				public := docDetail.GetPublic()
-				if public == 0 {
+				// 判断文档是否为公开的。0 为私密，1 为公开
+				if docDetail.Data.Public == 0 {
 					eds.ExceptionDocs = append(eds.ExceptionDocs, ExceptionDoc{
 						Title: docDetail.Data.Title,
 						Slug:  docDetail.Data.Slug,
 					})
 				}
 			}
+
 			<-concurrencyControl
 		}(toc)
 
